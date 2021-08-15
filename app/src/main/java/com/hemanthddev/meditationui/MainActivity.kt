65a9ba855910ddb4ae270d6571e95fabe933e0cb
Hemanth Kumar <hemanth@vyaparapp.in>
2021-08-15 07:10:13 +0530

    init

---
+++ app/src/main/java/com/hemanthddev/meditationui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.hemanthddev.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.hemanthddev.meditationui.ui.theme.HomeScreen
+import com.hemanthddev.meditationui.ui.theme.MeditationUITheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
+
---
+++ app/src/main/java/com/hemanthddev/meditationui/ui/theme/Theme.kt
@@ -0,0 +1,20 @@
+package com.hemanthddev.meditationui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MeditationUITheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
